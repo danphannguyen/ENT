23dada5256242caaed0d3d3ecb21c6d5ebefac1c
Dan PN <77532827+Derlvx@users.noreply.github.com>
2024-01-02 18:53:28 +0100

    feat : profile page front

--- profile.php
+++ profile.php
@@ -78,26 +78,205 @@
         <div id="profileBodyContainer">
 
             <div id="profileBodyDiv1">
-                <img src="./svg/profile.svg" alt="">
-                <h1>Prénom Nom</h1>
-                <div class="hr"></div>
-                <div>
-                    <img src="" alt="">
-                    <span>IUT Marne la Vallée - Campus Champs sur Marne</span>
+
+                <div class="profileBodyLabel">
+                    <h1>Coordonnées</h1>
                 </div>
-                <div>
-                    <img src="" alt="">
-                    <span>BUT MMI 2 - TPB</span>
+
+                <div class="profileBodyEdit">
+                    <img src="./svg/edit.svg" alt="">
                 </div>
+
+                <div id="profileInfo1">
+                    <img id="profilePicture" src="./svg/profile.svg" alt="">
+                    <h1>Prénom Nom</h1>
+                    <div class="hr"></div>
+
+                    <div class="profileTextContainer">
+                        <div class="profileText">
+                            <img class="profileIcon" src="./svg/school.svg" alt="">
+                            <span>IUT Marne la Vallée - Campus Champs sur Marne</span>
+                        </div>
+                        <div class="profileText">
+                            <img class="profileIcon" src="./svg/cours.svg" alt="">
+                            <span>BUT MMI 2 - TPB</span>
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
 
             <div id="profileBodyDiv2">
+
+                <div class="profileBodyLabel">
+                    <h1>Sécurité du Compte</h1>
+                </div>
+
+                <div class="profileBodyEdit">
+                    <img src="./svg/edit.svg" alt="">
+                </div>
+
+                <div id="profileInfo2">
+                    <div class="profileInfo2Text">
+                        <span>Identifiant</span>
+                        <span class="regularText">Prénom Nom</span>
+                    </div>
+                    <div class="hr"></div>
+                    <div class="profileInfo2Text">
+                        <span>Mot de passe</span>
+                        <span class="regularText">******************</span>
+                    </div>
+                    <div class="hr"></div>
+                    <div class="profileInfo2Text">
+                        <span>Courriel Universitaire</span>
+                        <span class="regularText">patrick.faure@edu.univ-eiffel.fr</span>
+                    </div>
+                    <div class="hr"></div>
+                    <div class="profileInfo2Text">
+                        <span>Téléphone</span>
+                        <span class="regularText">+33 6 95 37 02 85</span>
+                    </div>
+                </div>
 
             </div>
 
         </div>
 
         <div id="profileAsideContainer">
+
+            <div class="asideDiv">
+                <div class="asideDivTitle">
+                    <h1>Détails d’absences</h1>
+                </div>
+
+                <div class="asideBodyContainer">
+
+                    <div class="absTemplate">
+                        <div class="absPartOne">
+                            <img class="absIcon" src="./svg/right.svg" alt="">
+                            <span>Absence vendredi 1er décembre</span>
+                        </div>
+
+
+                        <div class="absPartTwo">
+                            <span>De 8h15 à 10h15 - 2 heures</span>
+                        </div>
+
+                    </div>
+
+                    <div class="absTemplate">
+                        <div class="absPartOne">
+                            <img class="absIcon" src="./svg/right.svg" alt="">
+                            <span>Absence vendredi 1er décembre</span>
+                        </div>
+
+
+                        <div class="absPartTwo">
+                            <span>De 8h15 à 10h15 - 2 heures</span>
+                        </div>
+
+                    </div>
+
+                    <div class="absTemplate">
+                        <div class="absPartOne">
+                            <img class="absIcon" src="./svg/right.svg" alt="">
+                            <span>Absence vendredi 1er décembre</span>
+                        </div>
+
+
+                        <div class="absPartTwo">
+                            <span>De 8h15 à 10h15 - 2 heures</span>
+                        </div>
+
+                    </div>
+
+                    <div class="absTemplate">
+                        <div class="absPartOne">
+                            <img class="absIcon" src="./svg/right.svg" alt="">
+                            <span>Absence vendredi 1er décembre</span>
+                        </div>
+
+
+                        <div class="absPartTwo">
+                            <span>De 8h15 à 10h15 - 2 heures</span>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div id="absSeeMore">
+                    <img src="./svg/threeDots.svg" alt="">
+                </div>
+
+            </div>
+
+            <div class="asideDiv">
+                <div class="asideDivTitle">
+                    <h1>Récapitulatif d’absences</h1>
+                </div>
+
+                <div class="asideBodyContainer">
+
+                    <div class="absTemplate">
+                        <div class="absPartOne">
+                            <img class="absIcon" src="./svg/right.svg" alt="">
+                            <span>Absence vendredi 1er décembre</span>
+                        </div>
+
+
+                        <div class="absPartTwo">
+                            <span>De 8h15 à 10h15 - 2 heures</span>
+                        </div>
+
+                    </div>
+
+                    <div class="absTemplate">
+                        <div class="absPartOne">
+                            <img class="absIcon" src="./svg/right.svg" alt="">
+                            <span>Absence vendredi 1er décembre</span>
+                        </div>
+
+
+                        <div class="absPartTwo">
+                            <span>De 8h15 à 10h15 - 2 heures</span>
+                        </div>
+
+                    </div>
+
+                    <div class="absTemplate">
+                        <div class="absPartOne">
+                            <img class="absIcon" src="./svg/right.svg" alt="">
+                            <span>Absence vendredi 1er décembre</span>
+                        </div>
+
+
+                        <div class="absPartTwo">
+                            <span>De 8h15 à 10h15 - 2 heures</span>
+                        </div>
+
+                    </div>
+
+                    <div id="absRecap">
+                        <div class="recapTemplate">
+                            <div class="recapPartOne">
+                                <span>Total d’absences :</span>
+                            </div>
+
+
+                            <div class="recapPartTwo">
+                                <span>12h30</span>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+            </div>
 
         </div>
 
