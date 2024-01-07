b1167b9d616fd57a304a202bd6c206ac66d422bd
Dan PN <77532827+Derlvx@users.noreply.github.com>
2024-01-07 18:34:10 +0100

    ade & crous final commit

--- CROUS/crous.php
+++ CROUS/crous.php
@@ -3,73 +3,74 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="styles.css" />
     <title>CROUS</title>
 </head>
 <body>
+
+  <nav>
+
+    <div class="navTop">
+      <div class="navUnivLogo">
+        <img id="navIconLogo" src="./svg/SymbLogo.svg" alt="">
+      </div>
+    </div>
+
+    <div id="navMiddle">
+      <a href="./accueil.php">
+        <div class="navIconContainer">
+          <div class="navIconBg">
+            <img src="./svg/accueil.svg" alt="">
+          </div>
+          <span>Accueil</span>
+        </div>
+      </a>
+      <a href="#">
+        <div class="navIconContainer">
+          <div class="navIconBg">
+            <img src="./svg/dashboard.svg" alt="">
+          </div>
+          <span>Dashboard</span>
+        </div>
+      </a>
+      <a href="#">
+        <div class="navIconContainer">
+          <div class="navIconBg">
+            <img src="./svg/calendirer.svg" alt="">
+          </div>
+          <span>Calendrier</span>
+        </div>
+      </a>
+      <a href="#">
+        <div class="navIconContainer">
+          <div class="navIconBg">
+            <img src="./svg/cours.svg" alt="">
+          </div>
+          <span>Cours</span>
+        </div>
+      </a>
+      <a href="#">
+        <div class="navIconContainer">
+          <div class="navIconBg">
+            <img src="./svg/chat.svg" alt="">
+          </div>
+          <span>Chat</span>
+        </div>
+      </a>
+    </div>
+
+    <div class="navBottom">
+      <a href="./profile.php">
+        <div class="navIconBg2">
+          <img src="<?php echo $result[0]['photo_user']; ?>" alt="">
+        </div>
+      </a>
+    </div>
+
+  </nav>
+
     <div class="container">
-        <nav>
-            <div class="navTop">
-                <a href="">
-                    <div class="navIconBg2">
-                        <img id="navIconLogo" src="./svg/SymbLogo.svg" alt="">
-                    </div>
-                </a>
-            </div>
-    
-            <div id="navMiddle">
-                <a href="#">
-                    <div class="navIconContainer">
-                        <div class="navIconBg">
-                            <img src="./svg/accueil.svg" alt="">
-                        </div>
-                        <span>Accueil</span>
-                    </div>
-                </a>
-                <a href="#">
-                    <div class="navIconContainer">
-                        <div class="navIconBg">
-                            <img src="./svg/dashboard.svg" alt="">
-                        </div>
-                        <span>Dashboard</span>
-                    </div>
-                </a>
-                <a href="#">
-                    <div class="navIconContainer">
-                        <div class="navIconBg">
-                            <img src="./svg/calendirer.svg" alt="">
-                        </div>
-                        <span>Calendrier</span>
-                    </div>
-                </a>
-                <a href="#">
-                    <div class="navIconContainer">
-                        <div class="navIconBg">
-                            <img src="./svg/cours.svg" alt="">
-                        </div>
-                        <span>Cours</span>
-                    </div>
-                </a>
-                <a href="#">
-                    <div class="navIconContainer">
-                        <div class="navIconBg">
-                            <img src="./svg/chat.svg" alt="">
-                        </div>
-                        <span>Chat</span>
-                    </div>
-                </a>
-            </div>
-    
-            <div class="navBottom">
-                <a href="">
-                    <div class="navIconBg2">
-                        <img src="./svg/profile.svg" alt="">
-                    </div>
-                </a>
-            </div>
-    
-        </nav>
+
         <section class="main">
             <div class="ade">
                 <table class="content-table">
